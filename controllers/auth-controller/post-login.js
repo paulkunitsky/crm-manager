b4975a3f88ba0be6ef08f30a4c43eccfaa360082
pkunitsky <let.it.music@gmail.com>
2018-07-14 14:22:32 +0300

    qweqwe

--- controllers/auth-controller/post-login.js
+++ controllers/auth-controller/post-login.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const {Collections, Config} = require('../../constants');
 const User = mongoose.model(Collections.USERS);
@@ -12,7 +13,7 @@ module.exports.postLogin = async function (req, res) {
 
     if (passwordsMatch) {
       const token = jwt.sign({userId: candidate._id}, Config.JWT_KEY, {expiresIn: '1 hour'});
-      res.status().json({message: 'Успешная авторизация'});
+      res.status(200).json({message: 'Успешная авторизация', token: `Bearer ${token}`});
     } else {
       res.status(401).json({error: 'Наверные данные для авторизации'});
     }
