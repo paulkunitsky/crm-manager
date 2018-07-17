89a3f41149615afffccfcd6c10b4889a453fa4a5
pkunitsky <let.it.music@gmail.com>
2018-07-17 15:27:03 +0300

    12

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -3,12 +3,15 @@ import {CommonModule} from '@angular/common';
 import {AuthRoutingModule} from './auth-routing.module';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
-import { AuthComponent } from './auth.component';
+import {AuthComponent} from './auth.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    // FormsModule,
   ],
   declarations: [
     LoginComponent,
